61148363b17b62a58822fc69ed37abc65cc91a5a
DFR <55308167+felixrimbach@users.noreply.github.com>
2025-06-03 14:57:14 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,13 @@ export default function Page() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-violet-50 to-violet-100 flex flex-col">
       <header className="py-10 text-center bg-white  shadow-lg">
-        <h1 className="text-3xl md:text-4xl font-bold">Product Order Form</h1>
+        <h1 className="text-3xl md:text-4xl font-bold">Device Rental Payment Form</h1>
       </header>
       <main className="flex-1 flex flex-col items-center justify-center px-6 w-screen my-10 md:my-0">
         <OrderForm />
       </main>
       <footer className="text-center text-gray-500 text-sm py-6 bg-white">
-        © 2025 Your Company. All rights reserved.
+        © Globibo Events.Studio. All rights reserved.
       </footer>
     </div>
   );
