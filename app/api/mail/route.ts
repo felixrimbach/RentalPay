6d871e699283cce978e3d0b2efdc6045969095e0
DFR <55308167+felixrimbach@users.noreply.github.com>
2025-06-11 16:31:04 +0800

    Update route.ts

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -26,17 +26,22 @@ export async function POST(request: NextRequest) {
                     <base target="_top">
                     </head>
                     <body>
+                    <p style="font-size: 16pt; text-align: left;">Receipt</p>
+                    <p style="font-size: 16pt; text-align: left;"></p>
                     <p style="font-size: 16pt; text-align: left;">Dear ${userName},</p>
-                    <p style="text-align: left;">This is to confirm that your payment of ${totalPrice} USD for ${quantity} unit(s) has been received.</p>
-                    <p style="text-align: left;">Transaction details:</p>
-                    <div style="font-size: 8pt; background-color: #f9f9f9; border-left: 4px solid #007BFF; padding: 15px; font-family: 'Courier New', monospace; white-space: pre-wrap;">
+                    <p style="font-size: 16pt; text-align: left;">This is to confirm that your payment of ${totalPrice} USD for ${quantity} unit(s) has been received.</p>
+                    <p style="font-size: 16pt; text-align: left;">Transaction details:</p>
+                    <div style="font-size: 16pt; background-color: #f9f9f9; border-left: 4px solid #007BFF; padding: 15px; font-family: 'Courier New', monospace; white-space: pre-wrap;">
                     Number of units: ${quantity}
                     Amount paid: ${totalPrice} USD
                     Transaction ID: ${transactionId}
                     Date / Time: ${formatted}
                     </div>
-                    <p style="text-align: left;">If you have any questions or concerns, please contact us at <a href="mailto:payment@globibo.com">payment@globibo.com</a>.</p>
-                    <p style="text-align: left;">Thank you for your business.</p>
+                    <p style="font-size: 16pt; text-align: left;">Please note that USD 70 per device will be refunded once the device(s) is/are returned in a good condition.</p>
+                    <p style="font-size: 16pt; text-align: left;">If you have any questions or concerns, please contact us at <a href="mailto:payment@globibo.com">payment@globibo.com</a>.</p>
+                    <p style="textfont-size: 16pt; text-align: left;">Thank you for your business.</p>
+                    <p style="textfont-size: 16pt; text-align: left;"></p>                    
+                    <p style="textfont-size: 16pt; text-align: left;">Globibo Pte. Ltd. / 114 Lavender Street 06-63, 338729 Singapore</p>
                     </body>
                     </html>
                     `
@@ -44,8 +49,8 @@ export async function POST(request: NextRequest) {
         const msg = {
             to: email,
             from: 'payment@globibo.com', // Change to your verified sender
-            subject: 'Your Payment has been received',
-            text: 'Your Payment has been received',
+            subject: 'Receipt: Your Payment has been received',
+            text: 'Receipt: Your Payment has been received',
             html: html,
         };
 
