5801967bfa015c9021d657d6a2f92a441b140b2a
DFR <55308167+felixrimbach@users.noreply.github.com>
2025-08-03 18:22:08 +0800

    Update OrderFormTest.tsx

--- components/OrderFormTest.tsx
+++ components/OrderFormTest.tsx
@@ -179,5 +179,16 @@ export default function OrderFormTest() {
         setAgreeTerms={setAgreeTerms}
       />
     </div>
+    <div id="google_translate_element"></div>
+        <script type="text/javascript">
+          function googleTranslateElementInit() {
+            new google.translate.TranslateElement(
+              {pageLanguage: "en"},
+              "google_translate_element"
+            );
+          }
+        </script>
+        <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
+        </script>
   );
 } 
