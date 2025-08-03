2198e0f61d8ab3503a35bd26acc29de0c06fc138
DFR <55308167+felixrimbach@users.noreply.github.com>
2025-08-03 18:18:28 +0800

    Update page.tsx

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -7,7 +7,11 @@ export default function TestPage() {
     <div className="min-h-screen bg-gradient-to-br from-violet-50 to-violet-100 flex flex-col">
       <header className="py-10 text-center bg-[#268DBB] text-white shadow-lg">
         <h1 className="text-3xl md:text-4xl font-bold">Device Rental Payment Form (Test)</h1>
-        <div id="google_translate_element"></div>
+      </header>
+      <main className="flex-1 flex flex-col items-center justify-center px-6 w-screen my-10 md:my-0">
+        <OrderFormTest />
+      </main>
+              <div id="google_translate_element"></div>
         <script type="text/javascript">
           function googleTranslateElementInit() {
             new google.translate.TranslateElement(
@@ -19,11 +23,6 @@ export default function TestPage() {
         <script type="text/javascript" 
           src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
         </script>
-
-      </header>
-      <main className="flex-1 flex flex-col items-center justify-center px-6 w-screen my-10 md:my-0">
-        <OrderFormTest />
-      </main>
       <footer className="text-center text-gray-500 text-sm py-6 bg-white">
         <p><a href="/terms" className="underline">Terms</a></p><br/>
         © Globibo Events.Studio. All rights reserved.
