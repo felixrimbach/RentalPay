579ff6aa1aba66aeb9841309f30e31f21746a769
sourabh jain <sourabh.jain@globibo.com>
2025-08-05 12:25:54 +0530

    Live page --> google translate widget

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,42 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import OrderForm from "@/components/OrderForm";
 
 export default function Page() {
+  useEffect(() => {
+    // Tell TS to ignore type check for this
+    (window as any).googleTranslateElementInit = () => {
+      new (window as any).google.translate.TranslateElement(
+        {
+          pageLanguage: "en",
+          includedLanguages: "id,nl,en,de,fr,hi,it,ja,km,ko,zh-CN,ms,es,th,vi,yue",
+          layout: (window as any).google.translate.TranslateElement.InlineLayout.SIMPLE,
+           autoDisplay: false,              // Disable auto-popup (default: true)
+        },
+        "google_translate_element"
+      );
+    };
 
+    const script = document.createElement("script");
+    script.src =
+      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
   return (
     <div className="min-h-screen bg-gradient-to-br from-violet-50 to-violet-100 flex flex-col">
       <header className="py-10 text-center bg-[#268DBB] text-white shadow-lg">
-        <h1 className="text-3xl md:text-4xl font-bold">Device Rental Payment Form</h1>
+        <h1 className="text-3xl md:text-4xl font-bold">
+          Device Rental Payment Form
+        </h1>
+
+        <div className="absolute right-6 top-6 max-w-[150px] md:max-w-none">
+          <div id="google_translate_element"></div>
+        </div>
       </header>
       <main className="flex-1 flex flex-col items-center justify-center px-6 w-screen my-10 md:my-0">
         <OrderForm />
